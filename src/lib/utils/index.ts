937d07af9a50dd40c0740d3eebe235be9abd61f0
Evan Schoffstall <evan.schoffstall@gmail.com>
2026-02-21 22:59:54 -0500

    refactor(hooks): relocate useIsMobile to core clientHooks and update exports
    Groups the hook move and all import/export rewiring needed to keep consumers working. This is a structural refactor (deleting the old hooks file, adding the hook to clientHooks, and updating sidebar + utils barrel) and should be atomic to avoid intermediate broken builds.

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,11 +1,11 @@
+export { useIsMobile } from "../core/clientHooks";
 export {
   DEFAULT_CATEGORY_LABEL,
   isDefaultCategory,
   normalizeCategory,
 } from "./categories";
 export { cn } from "./cn";
-export { useIsMobile } from "./hooks";
 export { parseOpmlFeedImport } from "./opml";
 export type { OpmlFeedImportEntry } from "./opml";
-export { multiLine } from "./textUtils";
+export { multiLine } from "./text-utils";
 export { normalizeFeedUrl, tryNormalizeFeedUrl } from "./url";
